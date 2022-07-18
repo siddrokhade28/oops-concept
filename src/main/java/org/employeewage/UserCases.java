2356ceab141070cb777bb07ecae3fd6e24d4e4f2
Siddanth Rokhade <siddrokhade@gmail.com>
2022-07-18 22:07:07 +0530

    Added:UC3

--- src/main/java/org/employeewage/UserCases.java
+++ src/main/java/org/employeewage/UserCases.java
@@ -4,7 +4,7 @@ public class UserCases {
     //Assume Full Day Hour is 8
     private static final int fullDayHour = 8;
     //Assume part-time Hour is 4;
-    private static final int partTime = 4;
+    private static final int partTimeHour = 4;
     //Assume Wage per Hour is 20
     private static final int wagePerHour = 20;
     //Checks weather Employee is present or not.
@@ -26,7 +26,23 @@ public class UserCases {
             int dailywage= fullDayHour*wagePerHour;
             System.out.println("Daily  Wage is "+dailywage);
         }
+        else {
+            System.out.println("Part-Time Wage is 0");
+        }
     }
-
+    public  void partTimeWage(){
+        double empcheck= Math.floor(Math.random()*10)%3;
+        if(empcheck==Is_full_time){
+            int dailywage= fullDayHour*wagePerHour;
+            System.out.println("Daily  Wage is "+dailywage);
+        }
+        else if(empcheck==Is_Part_Time){
+            int partTimeWage= partTimeHour*wagePerHour;
+            System.out.println("Part-Time Wage is "+partTimeWage);
+        }
+        else {
+            System.out.println("Part-Time Wage is 0");
+        }
+    }
 
 }
