e6fc5cf17613c194a934b328ff0cfc9aa2c1a684
Siddanth Rokhade <siddrokhade@gmail.com>
2022-07-18 23:45:20 +0530

    Added:line comparator program

--- src/main/java/org/linecomparator/Methods.java
+++ src/main/java/org/linecomparator/Methods.java
@@ -3,15 +3,21 @@ package org.linecomparator;
 import java.util.Scanner;
 
 public class Methods {
-    public void acceptInput(){
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter the X1 Coordinate");
-        double x1= s.nextDouble();
-        System.out.println("Enter the Y1 Coordinate");
-        double y1= s.nextDouble();
-        System.out.println("Enter the X2 Coordinate");
-        double x2= s.nextDouble();
-        System.out.println("Enter the Y2 Coordinate");
-        double y2= s.nextDouble();
+
+    public  double distanceCalculator(double x1, double x2,double y1,double y2){
+        Double length = Math.sqrt((Math.pow((x2-x1),2)+(Math.pow((y2-y1),2))));
+        return length;
+    }
+    public void comparator(String line1 , String line2){
+        if(line1.compareTo(line2) >0){
+            System.out.println("line 1 is greater");
+
+        } else if (line1.compareTo(line2)<0) {
+
+            System.out.println("line 2 is greater");
+        }
+        else {
+            System.out.println("lines are equal");
+        }
     }
 }
