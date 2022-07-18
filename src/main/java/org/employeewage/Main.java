1e10512bb25d3eaec04f23a723837997beb9413b
Siddanth Rokhade <siddrokhade@gmail.com>
2022-07-18 18:29:24 +0530

    Added:UC1

--- src/main/java/org/employeewage/Main.java
+++ src/main/java/org/employeewage/Main.java
@@ -2,6 +2,8 @@ package org.employeewage;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("----------Welcome to Employee Wage Computation Program----------");
+        UserCases userCases = new UserCases();
+        userCases.empChecker();
     }
 }
